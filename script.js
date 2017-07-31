e647aeacb961e47131d8e557fc6b5088cce7f60a
abohannon <abo46n2@gmail.com>
2017-07-30 20:39:59 -0700

    added general button click functionality and button sounds

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-document.addEventListener("DOMContentLoaded", function(){
+  const gameButtons = document.querySelectorAll('.push');
 
-  console.log("Hello world!");
+  const soundTL = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
+  const soundTR = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+  const soundBL = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+  const soundBR = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
 
+  Array.from(gameButtons).forEach(function(buttons){
+    buttons.addEventListener('click', buttonFunc);
+  });
 
+  function buttonFunc(e){
+    console.log(e.target);
+    if (e.target.id === 'tl') {
+      playSound(soundTL);
+    }
+    if (e.target.id === 'tr') {
+      playSound(soundTR);
+    }
+    if (e.target.id === 'bl') {
+      playSound(soundBL);
+    }
+    if (e.target.id === 'br') {
+      playSound(soundBR);
+    }
 
-});
+  }
+
+  function playSound(e){
+    e.play();
+  }
