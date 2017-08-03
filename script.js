5056571ed293cac2c408678d491a74734c7c9cf2
abohannon <abo46n2@gmail.com>
2017-08-02 21:19:22 -0700

    started work on computer AI

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
   const yellowBtn = document.querySelector('#yellow');
   const blueBtn = document.querySelector('#blue');
 
+  const buttonArray = [greenBtn, redBtn, yellowBtn, blueBtn];
+
   const startBtn = document.querySelector('#start');
   const strictBtn = document.querySelector('#strict');
   const strictLED = document.querySelector('.strict-led');
@@ -19,6 +21,8 @@
   let gameStart = false;
   let strict = false;
   let counter = 0;
+  let computerMoves = [];
+  let playerMoves = [];
 
   Array.from(gameButtons).forEach(buttons => {
     buttons.addEventListener('click', outerButtons);
@@ -27,9 +31,9 @@
   // General button behavior (sounds, active state, delay)
   function buttonFunc(eventVar, soundVar, activeClass){
     playSound(soundVar);
-    eventVar.target.classList.add(activeClass);
+    eventVar.classList.add(activeClass);
     setTimeout(function(){
-      eventVar.target.classList.remove(activeClass);
+      eventVar.classList.remove(activeClass);
     }, 250);
   }
 
@@ -40,18 +44,19 @@
       let buttonID = e.target.dataset.idx;
 
       if (e.target.id === 'green') {
-        buttonFunc(e, soundGreen, 'green-active');
+        buttonFunc(e.target, soundGreen, 'green-active');
       }
       if (e.target.id === 'red') {
-        buttonFunc(e, soundRed, 'red-active');
+        buttonFunc(e.target, soundRed, 'red-active');
       }
       if (e.target.id === 'yellow') {
-        buttonFunc(e, soundYellow, 'yellow-active');
+        buttonFunc(e.target, soundYellow, 'yellow-active');
       }
       if (e.target.id === 'blue') {
-        buttonFunc(e, soundBlue, 'blue-active');
+        buttonFunc(e.target, soundBlue, 'blue-active');
       }
       updateCount();
+      playerMoves.push(buttonID);
       console.log(counter);
     }
   }
@@ -63,6 +68,16 @@
   function updateCount() {
     counter++;
     display.innerHTML = counter;
+  }
+
+// TODO: Complete computer move function **experimenting**
+  function computerMove(){
+    buttonFunc(greenBtn, soundGreen, 'green-active');
+    computerMoves.push(greenBtn.dataset.idx);
+    setTimeout(function(){
+      buttonFunc(redBtn, soundRed, 'red-active');
+      computerMoves.push(redBtn.dataset.idx);
+    }, 1200);
   }
 
   startBtn.addEventListener('click', () => {
@@ -106,5 +121,7 @@
     display.innerHTML = '--';
     strictLED.classList.remove('strict-led-on');
     strict = false;
+    playerMoves = [];
+    computerMoves = [];
     console.clear();
   }
